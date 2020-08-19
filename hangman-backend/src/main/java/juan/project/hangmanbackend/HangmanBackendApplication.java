8e8c26c0d76788e003b32eb8971405cfacf2cde8
juan.eslava <juan.eslava@fdmgroup.com>
2020-08-19 16:14:47 +1200

    backend setup

---
+++ hangman-backend/src/main/java/juan/project/hangmanbackend/HangmanBackendApplication.java
@@ -0,0 +1,13 @@
+package juan.project.hangmanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangmanBackendApplication.class, args);
+	}
+
+}
